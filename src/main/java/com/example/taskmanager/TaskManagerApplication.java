447aae0d869136a126df505aecd2e91e1f71f39c
瞿俊生 <1019354192@qq.com>
2024-11-29 01:09:29 +0900

    1st version2 of demo for springboot+thymeleaf pj

--- src/main/java/com/example/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/example/taskmanager/TaskManagerApplication.java
@@ -2,7 +2,7 @@ package com.example.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// Author: Johnson
+// Author: JohnsonQ
 @SpringBootApplication
 public class TaskManagerApplication {
     public static void main(String[] args) {
